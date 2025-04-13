2ecd1d4692b7739f345ae9340966bb45134dbdea
FelixSanchez2902 <albabrens@gmail.com>
2025-04-12 23:29:54 -0400

    Final.

--- src/main/java/Final/DatabaseConnection.java
+++ src/main/java/Final/DatabaseConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/registros";
-        String usuario = "";
-        String password = "admin";
+        String url = "jdbc:mysql://localhost:3306/login_app_db";
+        String usuario = "root";
+        String password = "root";
 
         try {
             Connection connection = DriverManager.getConnection(url, usuario, password);
