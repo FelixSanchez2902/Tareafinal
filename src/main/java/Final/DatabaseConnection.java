7583aa12103eebe2db24a4c2b0921d87750b5008
FelixSanchez2902 <albabrens@gmail.com>
2025-04-12 23:24:45 -0400

    Final.

--- src/main/java/Final/DatabaseConnection.java
+++ src/main/java/Final/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/login_app_db";
+        String url = "jdbc:mysql://localhost:3306/registros";
         String usuario = "root";
         String password = "admin";
 
